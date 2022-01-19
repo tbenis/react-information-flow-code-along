23f576e2ea192bfa1c52de92df9848ef7f0d9674
Benis Tambe <will.jenifer@yahoo.com>
2022-01-18 19:13:15 -0500

    commit tree bf658eaa9363e19840bb99a8df11fc1e4aa36597

--- src/Child.js
+++ src/Child.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react'
 import { getRandomColor } from './randomColorGenerator.js'
 
 class Child extends Component {
+  // constructor(props) {
+  //   super();
+  //   this.state = {
+  //     color: getRandomColor(),
+  //     childrenColor: this.color
+  //   }
+  // }
   render() {
+    console.log("Propsss "+this.props)
     return (
       <div
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{backgroundColor: this.props.color}}
+        
+        onClick={ () => this.props.handleChangeColor(getRandomColor())}
       ></div>
     )
   }
